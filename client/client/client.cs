56ddb56c92f8d8ab6e773c08b5cc12921030de73
vano <davidevanoncini2003@gmail.com>
2021-05-20 11:52:44 +0200

    methods for starting, send and recive on both client and server

---
+++ client/client/client.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace client {
+    class client {
+        private Socket sender;
+        private byte[] bytes;
+        
+        public client() {
+            bytes = new byte[1024];
+        }
+
+        public void send(byte[] msg) {
+            int bytesSent = sender.Send(msg);
+        }
+
+        public void closeConnection() {
+            sender.Shutdown(SocketShutdown.Both);
+            sender.Close();
+        }
+
+        public string recive() {
+            int bytesRec = sender.Receive(bytes);
+            return Encoding.ASCII.GetString(bytes, 0, bytesRec);
+        }
+
+        public void StartClient(string newHost, int port) {
+            IPHostEntry host = Dns.GetHostEntry(newHost);
+            IPAddress ipAddress = host.AddressList[0];
+            IPEndPoint remoteEP = new IPEndPoint(ipAddress, port);
+            try {
+                sender = new Socket(ipAddress.AddressFamily,
+                    SocketType.Stream, ProtocolType.Tcp);
+                sender.Connect(remoteEP);
+
+            } catch (Exception e) {
+                Console.WriteLine(e.ToString());
+            }
+        }
+    }
+}
--- client/client/server.cs
+++ client/client/server.cs
@@ -6,9 +6,30 @@ using System.Text;
 namespace client {
     class server {
         private Socket listener;
+        private Socket handler;
 
         public void send(byte[] msg) {
+            handler.Send(msg);
+        }
 
+        public string recive() {
+            byte[] bytes = null;
+
+            string data = null;
+            while (true) {
+                bytes = new byte[1024];
+                int bytesRec = handler.Receive(bytes);
+                data += Encoding.ASCII.GetString(bytes, 0, bytesRec);
+                if (data.IndexOf("<EOF>") > -1) {
+                    break;
+                }
+            }
+            return data;
+        }
+
+        public void closeServer() {
+            handler.Shutdown(SocketShutdown.Both);
+            handler.Close();
         }
 
         public void StartServer(string newHost, int port) {
@@ -16,38 +37,15 @@ namespace client {
             IPAddress ipAddress = host.AddressList[0];
             IPEndPoint localEndPoint = new IPEndPoint(ipAddress, port);
             try {
-                Socket listener = new Socket(ipAddress.AddressFamily, SocketType.Stream, ProtocolType.Tcp);
+                listener = new Socket(ipAddress.AddressFamily, SocketType.Stream, ProtocolType.Tcp);
                 listener.Bind(localEndPoint);
                 listener.Listen(10);
-            }
 
                 Console.WriteLine("Waiting for a connection...");
-                Socket handler = listener.Accept();
-
-                string data = null;
-                byte[] bytes = null;
-
-                while (true) {
-                    bytes = new byte[1024];
-                    int bytesRec = handler.Receive(bytes);
-                    data += Encoding.ASCII.GetString(bytes, 0, bytesRec);
-                    if (data.IndexOf("<EOF>") > -1) {
-                        break;
-                    }
-                }
-
-                Console.WriteLine("Text received : {0}", data);
-
-                byte[] msg = Encoding.ASCII.GetBytes(data);
-                handler.Send(msg);
-                handler.Shutdown(SocketShutdown.Both);
-                handler.Close();
+                handler = listener.Accept();
             } catch (Exception e) {
                 Console.WriteLine(e.ToString());
             }
-
-            Console.WriteLine("\n Press any key to continue...");
-            Console.ReadKey();
         }
     }
 }
